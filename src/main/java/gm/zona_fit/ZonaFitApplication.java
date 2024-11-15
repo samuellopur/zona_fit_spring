87917f251f812134389e4ac68e24d10a72029027
Samuel Lopez <samuellopez01@hotmail.com>
2024-11-15 07:15:12 -0500

    Menu App Zona Fit

--- src/main/java/gm/zona_fit/ZonaFitApplication.java
+++ src/main/java/gm/zona_fit/ZonaFitApplication.java
@@ -8,11 +8,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class ZonaFitApplication implements CommandLineRunner {
 	@Autowired
 	private IClienteServicio clienteServicio;
 	private static final Logger logger = LoggerFactory.getLogger(ZonaFitApplication.class);
+	String ln = System.lineSeparator();
 
 	public static void main(String[] args) {
 		logger.info("Iniciando aplicación");
@@ -24,8 +27,31 @@ public class ZonaFitApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		logger.info("---------------------------------\n" +
-                    "*** Aplicación Zona Fit (GYM) ***\n" +
-                    "---------------------------------");
+
+		zonaFitApp();
 	}
+
+	private void zonaFitApp(){
+		var salir = false;
+		var consola = new Scanner(System.in);
+		while (!salir){
+			var opcion = mostrarMenu(consola);
+//			salir = ejecutarOpciones(consola, opcion);
+		}
+	}
+
+	private int mostrarMenu(Scanner consola){
+		logger.info("""
+		\n---------------------------------
+		*** Aplicación Zona Fit (GYM) ***
+		---------------------------------
+		1. Listar cliente
+		2. Buscar cliente
+		3. Agregar cliente
+		4. Modificar cliente
+		5. Eliminar cliente
+		6. Salir
+		Elige una opción:\s""");
+        return Integer.parseInt(consola.nextLine());
+    }
 }
